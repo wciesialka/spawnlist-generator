d8100b7bbfb43af765652438264c93452896806f
Mista-Epic <wciesialka@gmail.com>
2019-06-27 18:00:16 -0400

    Changed the way spawnlists are created, now will create children spawnlists for directories that hold directories

--- main.py
+++ main.py
@@ -16,6 +16,71 @@ UNSORTED_KEY = "Other"
 FILE_SEPERATOR_R = re.compile(r"[\/\\]")
 SANITISER_R = re.compile(r"[\<\>\:\"\/\\\|\?\*\s\'\`\_\+\=\!\@\#\$\%\^\&\;\,\.\~]")
 
+def populateSpawnlist(sl,modelList,i=1):
+
+    n = i + 2
+
+    unsorted = []
+    models = {}
+    childrenToCreate = []
+
+    for mdl in modelList:
+        split = FILE_SEPERATOR_R.split(mdl)
+        key = split[i]
+
+        if len(split) > n:
+            if not key in childrenToCreate:
+                childrenToCreate.append(key)
+        elif len(split) == n:
+            if not key in models:
+                models[key] = []
+            models[key].append(mdl)
+            modelList.remove(mdl)
+        elif len(split) == n-1:
+            unsorted.append(mdl)
+            modelList.remove(mdl)
+    
+    for childToCreate in childrenToCreate:
+        child = sl.createChild(childToCreate)
+        populateSpawnlist(child,modelList,i=i+1)
+
+    od = collections.OrderedDict(sorted(models.items(), key=lambda t: t[0]))
+
+    if len(unsorted) > 0:
+        od[UNSORTED_KEY] = unsorted
+
+    for k in od:
+        sl.addHeader(k)
+        v = od[k]
+        if not type(v) is str:
+            for m in od[k]:
+                sl.addModel(m)
+        else:
+            sl.addModel(v)
+
+def sanitiseName(name):
+    return SANITISER_R.sub("-",name)
+
+def saveSpawnlist(sl,path):
+    names = [sanitiseName(sl.name)]
+    slp = sl.parent
+    while not slp == None:
+        names.append(sanitiseName(slp.name))
+        slp = slp.parent 
+    cleanName = ""
+    for name in names[::-1]:
+        cleanName += name + "-"
+    filename = f"{sl.id:03d}-" + cleanName[:-1] + ".txt"
+    with open(os.path.join(path,filename),"w") as f:
+        try:
+            f.write(sl.as_string())
+        except IOError:
+            dialog.error_dialog("Failed to save spawnlist to file!",caption=CAPTION)
+        else:
+            dialog.ok_dialog("Successfully saved spawnlist!",caption=CAPTION)
+    for child in sl.children:
+        saveSpawnlist(child,path)
+
 def main():
     if dialog.ok_cancel_dialog("Select your spawnlist folder (Usually garrysmod/settings/spawnlist).",caption=CAPTION):
         path = dialog.dir_dialog(caption=CAPTION,defaultPath=config.get("DEFAULT","PreviousSpawnlistDirectory",fallback=""))
@@ -35,39 +100,21 @@ def main():
                     config["DEFAULT"]["PreviousVPKDirectory"] = os.path.dirname(vpkpath)
                     name = dialog.text_entry_dialog(message="Name your spawnlist.",caption=CAPTION)
                     if name and (not name==""):
-                        filename = f"{id:03d}-" + SANITISER_R.sub("-",name) + ".txt"
                         name = name.replace("\"","''")
-                        sl = spawnlist.Spawnlist(name,id=id)
+                    
                         pak = vpk.open(vpkpath)
-                        models = {}
-                        unsorted = []
+
+                        sl = spawnlist.Spawnlist(name,id=id)
+
+                        models = []
                         for pakfile in pak:
                             if pakfile.endswith(".mdl"):
-                                split = FILE_SEPERATOR_R.split(pakfile)
-                                if len(split) > 2:
-                                    key = split[1]
-                                    if not key in models:
-                                        models[key] = []
-                                    models[key].append(pakfile)
-                                else:
-                                    unsorted.append(pakfile)
+                                models.append(pakfile)
 
-                        od = collections.OrderedDict(sorted(models.items(), key=lambda t: t[0]))
+                        populateSpawnlist(sl,models)
 
-                        od[UNSORTED_KEY] = unsorted
+                        saveSpawnlist(sl,path)
 
-                        for k in od:
-                            sl.addHeader(k)
-                            for v in od[k]:
-                                sl.addModel(v)
-
-                        with open(os.path.join(path,filename),"w") as f:
-                            try:
-                                f.write(sl.as_string())
-                            except IOError:
-                                dialog.error_dialog("Failed to save spawnlist to file!",caption=CAPTION)
-                            else:
-                                dialog.ok_dialog("Successfully saved spawnlist!",caption=CAPTION)
     with open("PreviousDirectories.cfg","w") as f:
         config.write(f)
 
