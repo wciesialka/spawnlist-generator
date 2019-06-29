507fd155ac5915f9e2d3e60718885ea4a9a87ced
Mista-Epic <wciesialka@gmail.com>
2019-06-28 22:08:04 -0400

    Major reworking.

--- spawnlist.py
+++ spawnlist.py
@@ -2,8 +2,11 @@ import valvedict
 
 class Spawnlist:
 
-    def __init__(self,name,id=1,parentid=0,icon="icon16/page.png"):
-        self.id = id
+    ID = 0
+
+    def __init__(self,name,parentid=0,icon="icon16/page.png"):
+        self.id = Spawnlist.ID
+        Spawnlist.ID += 1
 
         self.name = name
         self.parentid = parentid
@@ -20,17 +23,11 @@ class Spawnlist:
         self.children.append(child)
 
     def createChild(self,name,icon="icon16/page.png"):
-        child = Spawnlist(name,id=self.getNextID(),parentid=self.id,icon=icon)
+        child = Spawnlist(name,parentid=self.id,icon=icon)
 
         self.addChild(child)
 
         return child
-
-    def getNextID(self):
-        if(len(self.children) > 0):
-            return self.children[-1].id + 1
-        else:
-            return self.id + 1
 
     def __finishAddingContent(self):
         self.contentCounter += 1
@@ -50,24 +47,23 @@ class Spawnlist:
         self.__finishAddingContent()
 
     def as_dict(self):
-        return {"TableToKeyValues": {
+        return {
             "parentid": f"{self.parentid}",
             "icon": self.icon,
             "id": f"{self.id}",
             "contents": self.contents,
             "name": self.name,
             "version": "3"
-        }}
+        }
     
     def as_valve_dict(self):
-        return valvedict.dict_as_valve_dict(self.as_dict())
-
-    def as_string(self):
-        s = ""
-        lines = self.as_valve_dict()
-        for line in lines:
-            s += line + "\n"
-        return s
+        return valvedict.DictToKeyValues(self.as_dict())
 
     def __str__(self):
-        return self.as_string()
+        return self.as_valve_dict()
+
+    def __len__(self):
+        length = 1
+        for child in self.children:
+            length += len(child) 
+        return length
