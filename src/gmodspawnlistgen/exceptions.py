35b0d8fcfa6e86faa29663c134c6440dba1e2fef
Willow Ciesialka <wciesialka@gmail.com>
2026-06-30 21:18:04 -0500

    Added exceptions

--- src/gmodspawnlistgen/exceptions.py
+++ src/gmodspawnlistgen/exceptions.py
@@ -1,4 +1,4 @@
-class InvalidPathException(Exception):
+class InvalidAppPathException(Exception):
 
     def __init__(self, app, path, reason):
         super().__init__(f"Given {app} path \"{path}\" invalid: {why}")
@@ -6,13 +6,23 @@ class InvalidPathException(Exception):
         self.path = path
         self.reason = reason
 
-class InvalidSteamPathException(InvalidPathException):
+class InvalidSteamPathException(InvalidAppPathException):
 
     def __init__(self, path, reason):
         super().__init__("Steam", path, reason)
+
+class InvalidGarrysModPathException(InvalidAppPathException):
+
+    def __init__(self, path, reason):
+        super().__init__("Garry's Mod", path, reason)
 
 class ImproperTableFormatException(Exception):
 
     def __init__(self, table, message):
         super().__init__(message)
         self.table = table
+
+class NoSteamRootException(Exception):
+
+    def __init__(self, func):
+        super().__init__(f"Cannot call {func.__name__} without existing Steam root folder.")
